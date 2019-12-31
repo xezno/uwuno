37b00a58be62563150287799f1c56a3cb93fd041
Alex <alexnoir03@gmail.com>
2019-12-31 00:35:42 +0000

    Removed dumb file headers

--- src/js/NotificationHandler.js
+++ src/js/NotificationHandler.js
@@ -1,27 +1,3 @@
-/*
-    notificationHandler.js
-
-    Copyright (c) 2019 Alex Guthrie
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-*/
-
 class NotificationHandler {
     constructor() {
         this.notificationCenter = document.getElementById("notification-center");
@@ -68,7 +44,7 @@ class NotificationHandler {
                 <h1><i class="remixicon-${icon}-fill"></i> ${title}</h1>
                 <h2>${text}</h2>
             </article>
-        `;
+        `; // this is terrible and i'm screaming
 
         // Wait until we can show the notification (animate in)
         setTimeout(() => {
